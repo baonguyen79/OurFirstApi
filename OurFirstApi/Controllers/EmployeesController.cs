77742fc8663e153e49012e29d3824e4f6d95f3d0
nguyen,bao <bao.nguyen@ingramcontent.com>
2017-08-24 20:23:16 -0500

    working

--- OurFirstApi/Controllers/EmployeesController.cs
+++ OurFirstApi/Controllers/EmployeesController.cs
@@ -32,8 +32,7 @@ namespace OurFirstApi.Controllers
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine(ex.Message);
-                    Console.WriteLine(ex.StackTrace);
+                   
                     return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Query blew up");
                 }
             }
@@ -51,11 +50,11 @@ namespace OurFirstApi.Controllers
 
                     var result =
                         connection.Query<EmployeeListResult>("Select * From Employee where EmployeeId = @id",
-                            new {id = id}).FirstOrDefault();
+                            new { id = id }).FirstOrDefault();
 
                     if (result == null)
                     {
-                        return Request.CreateErrorResponse(HttpStatusCode.NotFound,$"Employee with the Id {id} was not found");
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, $"Employee with the Id {id} was not found");
                     }
 
                     return Request.CreateResponse(HttpStatusCode.OK, result);
@@ -63,6 +62,77 @@ namespace OurFirstApi.Controllers
                 catch (Exception ex)
                 {
                     return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+                }
+            }
+        }
+
+        //PUT api/employees/5
+        public HttpResponseMessage Put(int id, EmployeeListResult employee)
+        {
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Chinook"].ConnectionString))
+            {
+                try
+                {
+                    var rowsAffected = connection.Execute($@"Update Employee
+                                                                set FirstName = @firstName,
+                                                                    LastName = @lastName
+                                                             Where EmployeeId = @employeeId",
+
+                        new { firstName = employee.FirstName, lastName = employee.LastName, employeeId = id });
+                                       
+                }
+                catch (Exception ex)
+                {
+                        return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+                }
+            }
+
+            return Request.CreateResponse(HttpStatusCode.Accepted);
+        }
+
+        // POST api/values
+        public HttpResponseMessage Post(EmployeeListResult employee)
+        {
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Chinook"].ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    var rowsAffected = connection.Execute("Insert into Employee(FirstName, LastName) " +
+                                                          "Values(@FirstName, @LastName)",
+                        new { FirstName = employee.FirstName, LastName = employee.LastName });
+
+                }
+
+                catch (Exception ex)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+
+                }
+
+
+                return Request.CreateResponse(HttpStatusCode.Created);
+            }
+        }
+
+        // DELETE api/values/5
+        public HttpResponseMessage Delete(int id)
+        {
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Chinook"].ConnectionString))
+            {
+                //connection.Open();
+                var sqlDelete = $@"Delete From Employee where EmployeeId = @EmployeeId";
+
+                try
+                {
+                    var affectedRows = connection.Execute(sqlDelete, new { EmployeeId = id });
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                catch (Exception ex)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+
                 }
             }
         }
